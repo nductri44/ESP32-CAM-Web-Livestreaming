0ce80a02e003661c26a818059a0a85289ab0086d
Tri Nguyen <nductri44@gmail.com>
2023-04-13 13:50:31 +0700

    First commit

---
+++ main/camera_pins.h
@@ -0,0 +1,27 @@
+#ifndef CAMERA_PINS_H_
+#define CAMERA_PINS_H_
+
+#define CONFIG_BOARD_ESP32CAM_AITHINKER 1
+
+// AiThinker ESP32Cam PIN Map
+#if CONFIG_BOARD_ESP32CAM_AITHINKER
+#define CAM_PIN_PWDN 32
+#define CAM_PIN_RESET -1 //software reset will be performed
+#define CAM_PIN_XCLK 0
+#define CAM_PIN_SIOD 26
+#define CAM_PIN_SIOC 27
+
+#define CAM_PIN_D7 35
+#define CAM_PIN_D6 34
+#define CAM_PIN_D5 39
+#define CAM_PIN_D4 36
+#define CAM_PIN_D3 21
+#define CAM_PIN_D2 19
+#define CAM_PIN_D1 18
+#define CAM_PIN_D0 5
+#define CAM_PIN_VSYNC 25
+#define CAM_PIN_HREF 23
+#define CAM_PIN_PCLK 22
+#endif
+
+#endif
---
+++ main/my_data.h
@@ -0,0 +1,7 @@
+#ifndef MY_DATA
+#define MY_DATA
+
+#define SSID "Galaxy A02s"
+#define PASS "12345689"
+
+#endif
